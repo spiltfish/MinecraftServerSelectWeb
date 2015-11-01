af81165ad9bbe521e4c526315c7fc4b3d4bdca5a
Mary <spiltfish@aim.com>
2015-11-01 11:01:41 -0800

    add slug

--- mcselect/mcselect/models.py
+++ mcselect/mcselect/models.py
@@ -2,11 +2,12 @@ from django.db import models
 import uuid
 
 class Server(models.Model):
-    server_name = models.CharField(max_length=128, unique=True, default=uuid.uuid1)
+    server_name = models.CharField(max_length=128)
     is_enabled = models.BooleanField()
     server_type = models.CharField(max_length=128)
     server_version = models.CharField(max_length=128)
     is_deployed = models.BooleanField(default=False)
+    slug = models.SlugField(unique=True, default=uuid.uuid1)
 
     @classmethod
     def create(cls, server_name, server_type, server_version):
