a7df2c9ed799797f47f074f965f0afc966e59d78
Mary <spiltfish@aim.com>
2015-10-31 18:43:23 -0700

    logging

--- mcselect/mcselect/views.py
+++ mcselect/mcselect/views.py
@@ -19,7 +19,8 @@ class AddServer(TemplateView):
         with transaction.atomic():
             print("Creating new server.")
             print(request)
-
+            print(args)
+            print(kwargs)
             server = Server.create("test", "server_type", "server_version")
             server.save()
 
