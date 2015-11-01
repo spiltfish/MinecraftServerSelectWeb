2ba8b99f20b3defee2ca6d625a7db8f853085cc6
Mary <spiltfish@aim.com>
2015-10-31 17:00:43 -0700

    Edit redirect

--- mcselect/mcselect/views.py
+++ mcselect/mcselect/views.py
@@ -20,4 +20,4 @@ class AddServer(TemplateView):
             server = Server.create("test", "server_type", "server_version")
             server.save()
 
-        return HttpResponseRedirect("/servers-select")
+        return HttpResponseRedirect("/servers")
